c599d2cbcfead0e0362e9a0b848df26ca2a497f3
Will Alvarez <will.alvarez@gmail.com>
2018-11-21 19:32:49 -0600

    mapa de fraccionamiento

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,90 @@
 var mapa;
 var loquehacemos = document.getElementById("mapa");
+var markersArray = [];
+var infowindow;
+
+function createMarker(place) {
+  var mkColor = $("#quehay-mapa .servicio.active").data("servicio");
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: mapa,
+    position: place.geometry.location,
+    icon: "/assets/img/pin-" + mkColor + ".svg"
+  });
+  var lng = place.geometry.location.lng();
+  var lat = place.geometry.location.lat();
+  var src =
+    "https://maps.googleapis.com/maps/api/streetview?size=600x300&location=" +
+    lat +
+    "," +
+    lng +
+    "&key=AIzaSyB87VxKTCOjaD6TDRPBsZbBF8qGi3hOvB8";
+  var contentString = "";
+  contentString += '<div class="media">';
+  contentString +=
+    '  <div class="mr-3" style="background-image: url(' +
+    src +
+    '); background-repeat: no-repeat; background-size: cover; width: 88px; height: 88px;" ></div>';
+  contentString += '  <div class="media-body">';
+  contentString +=
+    '    <h5 class="mt-0"><a href="https://www.google.com/maps/place/?q=place_id:' +
+    place.place_id +
+    '" target="_blank">' +
+    place.name +
+    "</h5></a>";
+  contentString += "<p>" + place.vicinity + "</p>";
+  contentString += "  </div>";
+  contentString += "</div>";
+  markersArray.push(marker);
+  var infowindow = new SnazzyInfoWindow({
+    map: mapa,
+    marker: marker,
+    maxWidth: 380,
+    closeOnMapClick: true,
+    closeWhenOthersOpen: true,
+    content: contentString
+  });
+  google.maps.event.addListener(marker, "click", function() {
+    var bgColor = $("#quehay-mapa .servicio.active").data("servicio");
+    console.log(bgColor);
+    infowindow.open(mapa, this);
+
+    infowindow.setWrapperClass(bgColor);
+  });
+}
+function removeMarkers() {
+  for (i = 0; i < markersArray.length; i++) {
+    markersArray[i].setMap(null);
+  }
+}
+
+function callback(results, status) {
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
+    removeMarkers();
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+      console.log(results[i]);
+    }
+  }
+}
+
+function servicio(servicio) {
+  var service = new google.maps.places.PlacesService(mapa);
+  service.nearbySearch(
+    {
+      location: { lat: 25.464993, lng: -100.978545 },
+      radius: 5500,
+      type: [servicio]
+    },
+    callback
+  );
+}
 
 function loquehacemosMapa() {
   mapa = new google.maps.Map(document.getElementById("mapa"), {
     center: { lat: 25.464993, lng: -100.978545 },
     zoom: 15,
-    disableDefaultUI: true,
+    // disableDefaultUI: true,
     styles: [
       {
         featureType: "water",
@@ -73,9 +152,10 @@ function loquehacemosMapa() {
       }
     ]
   });
+
   var icons = {
     party: {
-      icon: '/assets/img/quehay-marcador-bautek.svg'
+      icon: "/assets/img/quehay-marcador-bautek.svg"
     }
   };
   var party_location = {
@@ -84,7 +164,7 @@ function loquehacemosMapa() {
   };
   console.log(party_location.position);
   var currentMarker = new google.maps.Marker({
-    icon: '/assets/img/quehay-marcador-bautek.svg',
+    icon: "/assets/img/quehay-marcador-bautek.svg",
     position: party_location.position,
     map: mapa
   });
@@ -167,7 +247,7 @@ function initMap() {
   });
   var icons = {
     party: {
-      icon: '/assets/img/quehay-marcador-bautek.svg'
+      icon: "/assets/img/quehay-marcador-bautek.svg"
     }
   };
   var party_location = {
@@ -175,18 +255,26 @@ function initMap() {
     type: "party"
   };
   var currentMarker = new google.maps.Marker({
-    icon: '/assets/img/quehay-marcador-bautek.svg',
+    icon: "/assets/img/quehay-marcador-bautek.svg",
     position: party_location.position,
     map: map
   });
   map.setZoom(14);
   map.panTo(currentMarker.position);
 }
-initMap();
-if (loquehacemos) {
-  loquehacemosMapa();
-}
+
 $(function() {
+  $("#quehay-mapa .servicio").on("click", function() {
+    $("#quehay-mapa .servicio").removeClass("active");
+    $(this).addClass("active");
+    var currentServ = $(this).data("servicio");
+    console.log(currentServ);
+    servicio(currentServ);
+  });
+  initMap();
+  if (loquehacemos) {
+    loquehacemosMapa();
+  }
   $("#menu").on("click", function() {
     $(this).toggleClass("active");
     $("#menu-overlay").toggleClass("active");
