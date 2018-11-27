f0997416d6186815fa1aa17769e16db7cf0bab8c
Will Alvarez <will.alvarez@gmail.com>
2018-11-26 20:18:13 -0600

    BTK / inmueble: base de datos 1

--- cms/generators/inmueble/inmueble.js
+++ cms/generators/inmueble/inmueble.js
@@ -1,45 +1,44 @@
-{
-	titulo_proyecto: '',
-	tipo_de_proyecto: '',
-	$logo_proyecto_type: 'image',
-	logo_proyecto: '',
-	$imagen_homepage_type: 'image',
-	imagen_homepage: '',
-	$descripcion_corta_proyecto_type: 'textarea',
-	descripcion_corta_proyecto: '',
-	$video_fullscreen_type: 'file',
-	video_fullscreen: '',
-	genero_del_proyecto: '',
-	año: '',
-	temporadas: '',
-	episodios: '',
-	nombre_del_cliente: '',
-	$logotipo_del_cliente_type: 'image',
-	logotipo_del_cliente: '',
-	$imagen_sinopsis_type: 'image',
-	imagen_sinopsis: '',
-	$texto_sinopsis_type: 'textarea',
-	texto_sinopsis: '',
-	trailer_url: '',
-	ver_proyecto_texto: '',
-	imagenes_backstage: [
+({
+	latitud: '',
+	longitud: '',
+	$imagen_principal_type: 'image',
+	imagen_principal: '',
+	titulo: '',
+	comentario: '',
+	subtitulo: '',
+	descripcion: '',
+	ubicacion: '',
+	direccion: '',
+	construccion: '',
+	terreno: '',
+	plantas: '',
+	recamaras: '',
+	baños: '',
+	inversion: '',
+	$caracteristicas_casa_min: 1,
+	caracteristicas_casa: [
+		{
+			caracteristica: ''
+		}
+	],
+	$caracteristicas_fracc_min: 1,
+	caracteristicas_fracc: [
+		{
+			caracteristica: ''
+		}
+	],
+	$imagenes_min: 1,
+	imagenes: [
 		{
 			$imagen_type: 'image',
 			imagen: ''
 		}
 	],
-	$premios_min: 1,
-	premios: [
+	$planos_min: 1,
+	planos: [
 		{
-			titulo: '',
-			$logo_type: 'image',
-			logo: '',
-			url: ''
-		}
-	],
-	tags: [
-		{
-			nombre_del_tag: ''
+			$plano_type: 'image',
+			plano: '',
 		}
 	]
-}
+})
